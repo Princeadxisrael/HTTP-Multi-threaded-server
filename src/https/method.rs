89bb7b384bd360224583e862b41bf8a4aff63331
aseneca <princeosuh@gmail.com>
2024-10-08 19:32:06 +0100

    implemented lifetime parameters

--- src/https/method.rs
+++ src/https/method.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 pub enum Method{
     GET,
     PUT,
@@ -9,3 +10,25 @@ pub enum Method{
     OPTIONS,
     TRACE
 }
+
+//parse the method from the string using Fromstr
+impl FromStr for Method{
+    type Err = MethodError;
+    //fn to recieve a str from request and convert into one of the method enum parameters
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s{
+            "GET"=>Ok(Self::GET),
+            "PUT"=>Ok(Self::GET),
+            "PATCH"=>Ok(Self::PATCH),
+            "POST"=>Ok(Self::POST),
+            "DELETE"=>Ok(Self::DELETE),
+            "HEAD"=>Ok(Self::HEAD),
+            "OPTIONS"=>Ok(Self::OPTIONS),
+            "TRACE"=>Ok(Self::TRACE),
+            _ =>Err(MethodError)
+            
+        }
+    }
+}
+
+pub struct MethodError;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use server::Server;
 use https::Method;
 use https::Request;
